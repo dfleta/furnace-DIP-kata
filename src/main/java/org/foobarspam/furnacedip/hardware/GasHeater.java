31bea46a78bfffd9d1da68912b0ba05aeba04882
David Gelpi <gelpiorama@gmail.com>
2025-04-24 15:19:28 +0200

    refactor(GasHeater): Añadir @Override a los métodos  implementados

--- src/main/java/org/foobarspam/furnacedip/hardware/GasHeater.java
+++ src/main/java/org/foobarspam/furnacedip/hardware/GasHeater.java
@@ -5,10 +5,12 @@ import org.foobarspam.furnacedip.types.RoomTemperature;
 
 public class GasHeater implements Heater {
 	
+	@Override
 	public void engage(RoomTemperature temperature){
 		temperature.incrementTemperature(1);
 	}
 	
+	@Override
 	public void disengage(RoomTemperature temperature){
 		temperature.incrementTemperature(-1);
 	}
--- src/main/java/org/foobarspam/furnacedip/hardware/RemoteCommandSensor.java
+++ src/main/java/org/foobarspam/furnacedip/hardware/RemoteCommandSensor.java
@@ -5,6 +5,7 @@ import org.foobarspam.furnacedip.types.RoomTemperature;
 
 public class RemoteCommandSensor implements Thermometer {
 	
+	@Override
 	public double read(RoomTemperature temperature){
 		return temperature.getTemperature();
 	}
